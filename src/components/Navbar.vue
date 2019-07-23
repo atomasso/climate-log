a27f12b7e756d4b4070310782b6b9c4e38943114
Jane Doe <tomas.ante@gmail.com>
2019-07-23 12:53:39 +0200

    Integrate Bootstrap component library

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="nav-bar">
-    <nav class="nav-extended indigo darken-2">
-      <div class="nav-content">
-        <router-link :to="{ name: 'TableView' }">
-          <span class="nav-title">Table</span>
-        </router-link> 
-        <router-link :to="{ name: 'ChartView' }">
-          <span class="nav-title">Chart</span>
-        </router-link>        
-      </div>
-    </nav>
-  </div>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <a class="navbar-brand" href="#">Climate Log</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarColor03">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="">
+            <router-link :to="{ name: 'TableView' }">
+              Table
+            </router-link> 
+          </a>       
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">
+            <router-link :to="{ name: 'ChartView' }">
+              Chart
+            </router-link> 
+          </a>    
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
