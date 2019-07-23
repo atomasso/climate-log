0c73d16004fa8ce32fc3d7e8598f655ae7d04cc7
Jane Doe <tomas.ante@gmail.com>
2019-07-23 11:44:56 +0200

    Add basic routing

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nav-bar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link :to="{ name: 'TableView' }">
+          <span class="nav-title">Table</span>
+        </router-link> 
+        <router-link :to="{ name: 'ChartView' }">
+          <span class="nav-title">Chart</span>
+        </router-link>        
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  components: {
+    
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TableView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="nav-bar">
+   Table
+  </div>
+</template>
+
+<script>
+// import TableView from '@/components/TableView'
+
+export default {
+  name: '',
+  components: {
+    
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
